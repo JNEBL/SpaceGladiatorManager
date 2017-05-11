7cf8b67c99b6a3a7ff1935f83d08dc853b7e717b
jnebl7236 <jneblparcore@gmail.com>
2017-05-11 12:27:46 -0500

    Universe 2

--- src/DevCheat/DevRunner.java
+++ src/DevCheat/DevRunner.java
@@ -3,17 +3,30 @@ package DevCheat;
 import Fighters.Gladiator.DataBaseGrabberGladiator.GladiatorDataBase;
 import GameFrameWork.*;
 import Player.Player.Player;
+import World.Galaxy.Universe.Universe;
 
 /**
  * Created by jnebl on 5/6/2017.
  */
 public class DevRunner {
     public static void main(String args[]){
-        Window window = new Window();
-        window.addMouseListener(new Mouse());
-        Window.menu.displayAll();
-
-
-
+        Universe universe = new Universe(10000);
+        System.out.println("Stars =" + universe.getStars().size());
+        int temp = 0;
+        for (int x = 0;x < universe.getStars().size();x++){
+            for (int y = 0;y < universe.getStars().get(x).getPlanets().size();x++){
+                temp += 1;
+            }
+        }
+        System.out.println("Planets = " + temp);
+        temp = 0;
+        for (int x = 0;x < universe.getStars().size();x++){
+            for (int y = 0;y < universe.getStars().get(x).getPlanets().size();x++){
+                for (int z = 0;z < universe.getStars().get(x).getPlanets().get(y).getMoons().size();x++){
+                    temp += 1;
+                }
+            }
+        }
+        System.out.println("Moons =" + temp);
     }
 }
