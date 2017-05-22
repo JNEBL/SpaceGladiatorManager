67d8a59570620932bce70701bd1062064c2926a6
jkennedy1998 <jkennedy1998@gmail.com>
2017-05-22 08:53:59 -0500

    got rid of some commments and two classes that dont work

--- src/Menus/MenuUniverseCreation.java
+++ src/Menus/MenuUniverseCreation.java
@@ -3,10 +3,6 @@ package Menus;
 import Calculations.Unigen;
 import GameFrameWork.Buttons.Button;
 import GameFrameWork.Buttons.ButtonNudge;
-import GameFrameWork.Buttons.ButtonSlider;
-import GameFrameWork.Panel;
-import GameFrameWork.PanelRedBox;
-import GameFrameWork.Window;
 import World.Galaxy.Universe.Universe;
 
 /**
@@ -17,7 +13,6 @@ public class MenuUniverseCreation extends Menu {
     private int stars = 50;
     public MenuUniverseCreation(){
         buttons.add(new Button(60,600,100,40,"Create Universe"));
-//        buttons.add(new ButtonSlider(60,200,500,25,"stars",10,stars));
         buttons.add(new ButtonNudge(60,200,100,40,"stars",50,1000,stars,50));
         //universe size
         //universe age?
@@ -36,13 +31,13 @@ public class MenuUniverseCreation extends Menu {
                     Unigen.universe = new Universe(stars);
                     System.out.println("Ended world generation");
                     System.out.println(Unigen.universe.getNumberOfStars()+" stars");
-//                    System.out.println(Unigen.universe.getNumberOfHyperSpaceLanes()+" hyperspace lanes");
-//                    System.out.println(Unigen.universe.xDimension+ " for x dimension");
-//                    System.out.println(Unigen.universe.yDimension+ " for y dimension");
-//                    System.out.println(Unigen.universe.zDimension+ " for z dimension");
-//                    System.out.println(Unigen.universe.getNumberOfPlanets()+" is the number of planets");
-//                    System.out.println(Unigen.universe.getNumberOfMoons()+" is the number of moons");
-//                    System.out.println(Unigen.universe.getNumberOfSpaceStations()+" is the number of space Stations");
+                    System.out.println(Unigen.universe.getNumberOfHyperSpaceLanes()+" hyperspace lanes");
+                    System.out.println(Unigen.universe.xDimension+ " for x dimension");
+                    System.out.println(Unigen.universe.yDimension+ " for y dimension");
+                    System.out.println(Unigen.universe.zDimension+ " for z dimension");
+                    System.out.println(Unigen.universe.getNumberOfPlanets()+" is the number of planets");
+                    System.out.println(Unigen.universe.getNumberOfMoons()+" is the number of moons");
+                    System.out.println(Unigen.universe.getNumberOfSpaceStations()+" is the number of space Stations");
 
 
 
