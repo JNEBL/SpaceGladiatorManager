66361eb98aad40d872364f93935d1cf267884dff
jkennedy1998 <jkennedy1998@gmail.com>
2017-05-22 10:58:14 -0500

    make a key listener

---
+++ src/GameFrameWork/Keyboard.java
@@ -0,0 +1,19 @@
+package GameFrameWork;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+/**
+ * Created by citim on 5/22/2017.
+ */
+public class Keyboard implements KeyListener {
+    public void keyPressed(KeyEvent e){
+
+    }
+    public void keyReleased(KeyEvent e){
+
+    }
+    public void keyTyped(KeyEvent e){
+
+    }
+}
