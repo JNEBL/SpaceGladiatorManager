959f44331a113a1d1c190e6f5312d0310c8bf6e3
jnebl7236 <jneblparcore@gmail.com>
2017-05-25 11:57:37 -0500

    ButtonMultiToggle

---
+++ src/GameFrameWork/Buttons/ButtonMultiToggle.java
@@ -0,0 +1,10 @@
+package GameFrameWork.Buttons;
+
+/**
+ * Created by student5 on 5/25/17.
+ */
+public class ButtonMultiToggle extends Button{
+    public ButtonMultiToggle(int x,int y,int xDimension,int yDimension,){
+        super(x,y,xDimension,yDimension);
+    }
+}
