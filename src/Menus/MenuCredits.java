80fab2f9e18b2b28446f48505e51c1ea79c328a7
jnebl7236 <jneblparcore@gmail.com>
2017-05-25 11:34:20 -0500

    update Letters

--- src/Menus/MenuCredits.java
+++ src/Menus/MenuCredits.java
@@ -12,7 +12,7 @@ public class MenuCredits extends Menu {
     public MenuCredits() {
         buttons.add(new Button(60, 600, 100, 40, "Back"));
         panels.add(new PanelTextBox(60,100,400,200,
-                "Made by Jared Kennedy and Justin Neble.",this));
+                "M ade by Jared Kennedy and Justin Neble.",this));
 
     }
 
